bedda045d4d8519f2f568ac815a08e2f7d955204
charan1147 <charanc1147@gmail.com>
2025-07-09 10:11:24 +0530

    last commit14

--- sockets/socket.js
+++ sockets/socket.js
@@ -10,7 +10,7 @@ const BASE_URL = "https://quizapi.io/api/v1/questions";
 export const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL||"http://localhost:5173",
+      origin: process.env.CLIENT_URL,
       methods: ["GET", "POST"],
       credentials: true,
     },
