d7223d2293cab622943758134a4c68c60678642e
charan1147 <charanc1147@gmail.com>
2025-07-11 12:52:04 +0530

    changes2

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
@@ -6,6 +7,11 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 6 },
   createdAt: { type: Date, default: Date.now },
 });
+
+
+userSchema.methods.comparePassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
